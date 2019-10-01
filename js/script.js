abdf8e9bde3fd0de3ee14421d72709135c1c3dce
Mary Gertrude <gertrude@Marys-MacBook-Air-4.local>
2019-10-01 22:00:05 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -37,7 +37,14 @@ var quotes = [
    - Cse the random number to `return` a random quote object from the `quotes` array.
 ***/
 
-
+function getRandomQuote(){
+  maxInt = quotes.length;
+  minInt = 0;
+  //formula below adapted from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random -->
+  randNum = Math.floor(Math.random() * (maxInt - minInt + 1) + minInt); 
+   //console.log(quotes[randNum].quote);
+  return quotes[randNum];
+}
 
 
 /***
